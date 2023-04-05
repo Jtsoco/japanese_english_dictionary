712940671242009ff291be103b4b064a9e677ff3
Jackson Socolofsky <jtsoco@gmail.com>
2023-04-05 15:42:07 +0900

    search improved

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -4,11 +4,13 @@ class WordsController < ApplicationController
     @words = []
     if params[:q].present?
       @query = params[:q]
+      # needed to get rid of japanese keyboard space
+      @query.gsub!("　", " ")
       @query.split.each do |word|
         full_words = Word.where("japanese = ?", word)
         # Empty checks if the word doesn't exist, nil? doesn't work
         full_words = Word.where("reading = ?", word) if full_words.empty?
-        full_words = Word.where("english = ?", word) if full_words.empty?
+        full_words = Word.where("english ILIKE ?", word) if full_words.empty?
         # TODO put error if word doesn't return what is wanted
         # where gives an array, which works as sometimes there are multiple words for a giving aspect
         @words << full_words
