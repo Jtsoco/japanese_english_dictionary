b00613973a35b8bade7126aa63829b174759a114
Jackson Socolofsky <jtsoco@gmail.com>
2023-04-05 18:07:35 +0900

    added comments

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -44,3 +44,15 @@ end
 
 # curl request example
 # curl -X POST http://localhost:3000/api -H "Content-Type: application/json" -H 'Accept: application/json' -d '{"info": {"language": "japanese", "array": ["食べる"]}}'
+
+
+# post example
+# curl -X POST http://localhost:3000/api -H "Content-Type: application/json" -H 'Accept: application/json' -d '{"info": {"language": "japanese", "array": ["食べる", "飲む"]}}'
+# post response
+# [[{"id":33490,"japanese":"食べる","english":"to eat","reading":"たべる","created_at":"2023-04-05T02:23:16.012Z","updated_at":"2023-04-05T02:23:16.012Z"}],[{"id":15375,"japanese":"飲む","english":"to drink","reading":"のむ","created_at":"2023-04-05T02:22:50.570Z","updated_at":"2023-04-05T02:22:50.570Z"}]]%
+
+# exmaple of english
+# curl -X POST http://localhost:3000/api -H "Content-Type: application/json" -H 'Accept: application/json' -d '{"info": {"language": "english", "array": ["food", "drink"]}}'
+
+# english response
+# [[{"japanese":"召し上がりもの","english":"food","reading":"めしあがりもの"},{"japanese":"食べ物","english":"food","reading":"たべもの"},{"japanese":"食品","english":"food","reading":"しょくひん"},{"japanese":"食物","english":"food","reading":"しょくもつ"},{"japanese":"食料","english":"food","reading":"しょくりょう"},{"japanese":"食い物","english":"food","reading":"くいもの"},{"japanese":"食","english":"food","reading":"しょく"},{"japanese":"糧","english":"food","reading":"かて"},{"japanese":"フード","english":"food","reading":"フード"}],[{"japanese":"ドリンク","english":"drink","reading":"ドリンク"},{"japanese":"飲み物","english":"drink","reading":"のみもの"},{"japanese":"飲み","english":"drink","reading":"のみ"}]]
