dae349214ba2a7d2c74e33fcc437291dbca4f8c8
Jackson Socolofsky <jtsoco@gmail.com>
2023-04-05 18:06:01 +0900

    backend api returns array of array, with each element in the first array being an array of all the entries that match the search, returned as hash

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'words#index'
   resources :words, only: [:index]
+  # resources :api, only: [:index]
+  post 'api', action: :index, controller: 'api', via: [:post]
 
   # Defines the root path route ("/")
   # root "articles#index"
