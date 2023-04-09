ca845ad6630b3f6d944b64825f7f61b4e6a1ae8b
Jackson Socolofsky <jtsoco@gmail.com>
2023-04-09 14:53:30 +0900

    added user model, has the ability to set a private api key via a callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  encrypts :private_api_key
+  blind_index :private_api_key
+  before_create :set_private_api_key
+
+  validates :private_api_key, uniqueness: true, allow_blank: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  private
+  def set_private_api_key
+    self.private_api_key = SecureRandom.hex if self.private_api_key.nil?
+  end
 end
